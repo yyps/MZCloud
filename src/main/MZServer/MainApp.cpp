24bcbe04eda283e1477b8daaa2c56a5af80c7957
guojianguang <guojianguang@waction.cn>
2022-01-18 14:54:30 +0800

    增加windows支持
    Signed-off-by: guojianguang <guojianguang@waction.cn>

--- src/main/MZServer/MainApp.cpp
+++ src/main/MZServer/MainApp.cpp
@@ -1,8 +1,30 @@
 #include "MainApp.h"
 #include "ServerErrdef.h"
 
+#ifdef _WIN32
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <windows.h>
+#include <getopt.h>
+#include <io.h>
+#include <fcntl.h>
+#else
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <dirent.h>
+#endif
+
 CMainApp::CMainApp()
 {
+#ifdef _WIN32
+    WORD wVersionRequested;
+    WSADATA wsaData;
+    wVersionRequested = MAKEWORD(2, 2);
+    WSAStartup(wVersionRequested, &wsaData);
+#endif
+
     m_httpSvr = std::make_shared<CEvHttpServer>();
 }
 
@@ -13,6 +35,7 @@ CMainApp::~CMainApp()
 
 int CMainApp::Init()
 {
+
     return ERR_SUCCESS;
 }
 
